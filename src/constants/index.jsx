a1e5c6cabdf0d70f27d558fe3fe652479c9a4494
rashmikaz <phrashmika@gmail.com>
2025-03-08 01:47:05 +0530

    workflow

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -99,25 +99,30 @@ export const features = [
 
 export const checklistItems = [
   {
-    title: "Code merge made easy",
+    title: "Concept",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "We help our Partners carry out the discovery phase, defining the product-market fit and forming the project requirements. Get started with business analytics, lean startup coaching and design thinking.",
   },
   {
-    title: "Review code without worry",
+    title: "Prototype",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "We create clickable product versions and help our Partners define further development, essential functionality and user design. According to the decisions made at this stage, we assign a professional team dedicated solely to your project.",
   },
   {
-    title: "AI Assistance to reduce time",
+    title: "MVP",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "Your dedicated team creates a basic yet fully functional version of the product ready for market launch. A minimum viable product helps you attract investments and gather invaluable user feedback to determine the next features.",
   },
   {
-    title: "Share work in minutes",
+    title: "Full Product",
     description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+      "With the user feedback from the MVP and our business expertise, your assigned dedicated team creates a full-fledged, marketable product. Our developers follow Scrum best practices and the agreed feature plan.",
   },
+  {
+    title: "Scaling",
+    description:
+      "We bet on long-term collaborations and guide our Partners through the entire process, including scaling already existing products. We help them find new niches and expand the product's market presence.",
+  }
 ];
 
 export const pricingOptions = [
