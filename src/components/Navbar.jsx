cca0d0b4293b361a8828ee7a69bbf9433f8e0fb2
rashmikaz <phrashmika@gmail.com>
2025-03-08 00:10:52 +0530

    nav bar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,17 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create an account
+            </a>
+          </div>
         </div>
      </div>
          
